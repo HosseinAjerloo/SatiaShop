b15bfc73b307fb4b3aed8decff8821fa0acb29ef
HosseinAjerloo <ahosseinajerloo@gmail.com>
2025-02-11 10:10:00 +0330

    change iameg

--- app/Services/ImageService/ImageService.php
+++ app/Services/ImageService/ImageService.php
@@ -7,38 +7,38 @@ use Intervention\Image\Drivers\Gd\Driver;
 
 class ImageService extends ImageProvider
 {
-    public function saveImage($image)
-    {
-        $result = $this->getObjFile()->move($this->getAddressFile(), $this->getFinalImageName());
-
-        $imageService = ImageManager::gd()->read($this->getFinalFileAddres());
-
-        dd($imageService);
-        $this->setFileName(time());
-        foreach (config('ImageConfig') as $key => $imagResize) {
-            dd($imagResize,$key);
-        }
-        dd($imageService);
-        $this->setObjFile($image);
-        $this->provider();
-        if ($result) {
-
-            return $this->getFinalFileAddres();
-        }
-        return false;
-    }
 //    public function saveImage($image)
 //    {
+//        $result = $this->getObjFile()->move($this->getAddressFile(), $this->getFinalImageName());
 //
+//        $imageService = ImageManager::gd()->read($this->getFinalFileAddres());
+//
+//        dd($imageService);
+//        $this->setFileName(time());
+//        foreach (config('ImageConfig') as $key => $imagResize) {
+//            dd($imagResize,$key);
+//        }
+//        dd($imageService);
 //        $this->setObjFile($image);
 //        $this->provider();
-//        $result=$this->getObjFile()->move($this->getAddressFile(),$this->getFinalImageName());
-//        if ($result)
-//        {
+//        if ($result) {
+//
 //            return $this->getFinalFileAddres();
 //        }
 //        return false;
 //    }
+    public function saveImage($image)
+    {
+
+        $this->setObjFile($image);
+        $this->provider();
+        $result=$this->getObjFile()->move($this->getAddressFile(),$this->getFinalImageName());
+        if ($result)
+        {
+            return $this->getFinalFileAddres();
+        }
+        return false;
+    }
     public function deleteImage($path)
     {
         if (file_exists($path)) {
