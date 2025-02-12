853ab6e3fe9d370a7c2ee5b7c28e53955c74c20c
HosseinAjerloo <ahosseinajerloo@gmail.com>
2025-02-12 09:21:15 +0330

    remove brand

--- resources/Shop/Panel/index.blade.php
+++ resources/Shop/Panel/index.blade.php
@@ -81,16 +81,16 @@
 
 
 
-    <section class="mb-3">
-        <section class="container-xxl">
-            <!-- one column -->
-            <section class="row py-4">
-                <section class="col"><img class="d-block rounded-2 w-100"
-                                          src="{{asset("shop/assets/images/ads/one-col-1.jpg")}}" alt=""></section>
-            </section>
+{{--    <section class="mb-3">--}}
+{{--        <section class="container-xxl">--}}
+{{--            <!-- one column -->--}}
+{{--            <section class="row py-4">--}}
+{{--                <section class="col"><img class="d-block rounded-2 w-100"--}}
+{{--                                          src="{{asset("shop/assets/images/ads/one-col-1.jpg")}}" alt=""></section>--}}
+{{--            </section>--}}
 
-        </section>
-    </section>
+{{--        </section>--}}
+{{--    </section>--}}
 
 
 
