468a93ddb27a9e4be5be8b234bd0b11e308f7e4f
HosseinAjerloo <ahosseinajerloo@gmail.com>
2025-02-22 15:44:11 +0330

    add sso to project

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -189,22 +189,21 @@ class LoginController extends Controller
             $responseUser = Http::withHeaders([
                 'Authorization' => $responseObj->token_type . ' ' . $responseObj->access_token,
             ])->get('https://oauth.satia.co/api/user');
-            dd($responseUser);
-            if($responseUser->successful()) {
-                $userObj = $responseUser->object();
-                $user = User::firstOrCreate([
-                    'mobile' => $userObj->mobile,
-                    'email'=>$userObj->user
-                ], [
-                   'name'=>$userObj->first_name??'',
-                    'family'=>$userObj->last_name??''
-                ]);
-
-                    auth()->login($user, true);
-                    session(['login_type' => 'sso']);
-                    session()->forget('state');
-                    return redirect()->route('panel.index');
-                }
+//            if($responseUser->successful()) {
+//                $userObj = $responseUser->object();
+//                $user = User::firstOrCreate([
+//                    'mobile' => $userObj->mobile,
+//                    'email'=>$userObj->user
+//                ], [
+//                   'name'=>$userObj->first_name??'',
+//                    'family'=>$userObj->last_name??''
+//                ]);
+//
+//                    auth()->login($user, true);
+//                    session(['login_type' => 'sso']);
+//                    session()->forget('state');
+//                    return redirect()->route('panel.index');
+//                }
             }
 
 
