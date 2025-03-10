863c51f5533e44ddf12fe18a058e5de0fcc9d285
HosseinAjerloo <ahosseinajerloo@gmail.com>
2025-03-10 15:42:02 +0330

    sync

--- app/Services/BankService/Meli.php
+++ app/Services/BankService/Meli.php
@@ -272,9 +272,12 @@ class Meli extends Service
             $arrres = $this->cullRequest('https://sadad.shaparak.ir/vpg/api/v0/Advice/Verify');
             if ($arrres->ResCode != -1 && $arrres->ResCode == 0) {
                 $Refid = $arrres->SystemTraceNo;
+                SystemTraceNo
                 if ($Refid == '') {
+                    dd('refID',$arrres);
                     return 1050 ;
                 }
+                dd('end if');
                 $RefNo = $arrres->RetrivalRefNo;
                 request()->request->add(['RefNum'=>$Token]);
                 return true ;
