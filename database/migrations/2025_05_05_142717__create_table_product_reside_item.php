f1187d24744f6e6247b30649a94f244ddcd4d84f
HosseinAjerloo <ahosseinajerloo@gmail.com>
2025-05-05 14:45:57 +0330

    sync

---
+++ database/migrations/2025_05_05_142717__create_table_product_reside_item.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_reside_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->nullable()->constrained('products')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('reside_item_id')->nullable()->constrained('reside_items')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->enum('balloons',['internal','external'])->nullable();
+            $table->decimal('salary',20,3)->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resideItems');
+    }
+};
