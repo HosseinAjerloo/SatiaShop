4ddcdd9c0688a41847d007e3d539baafca4cae40
HosseinAjerloo <ahosseinajerloo@gmail.com>
2025-05-20 14:14:44 +0330

    final project

--- resources/Shop/Admin/index.blade.php
+++ resources/Shop/Admin/index.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 
     <section class="px-3 space-y-5">
-        <section class="flex items-center justify-between ">
-            <a href="{{route('admin.resideCapsule.index')}}" class="border border-black/30 rounded-md  flex items-center justify-center flex-col w-[23%]">
+        <section class="flex items-center justify-between flex-wrap">
+            <a href="{{route('admin.resideCapsule.index')}}" class="border border-black/30 rounded-md  flex items-center justify-center flex-col sm:w-[23%] w-[30%] mt-2">
                 <div class="p-2">
                     <img src="{{asset("capsule/images/daryaft-capsule.svg")}}" alt="" class="w-14 h-14">
                 </div>
@@ -15,7 +15,7 @@
                     </p>
                 </div>
             </a>
-            <a href="{{route('admin.sale.index')}}" class="border border-black/30 rounded-md  flex items-center justify-center flex-col w-[23%]">
+            <a href="{{route('admin.sale.index')}}" class="border border-black/30 rounded-md  flex items-center justify-center flex-col sm:w-[23%] w-[30%] mt-2">
                 <div class="p-2">
                     <img src="{{asset("capsule/images/sale.png")}}" alt="" class="w-14 h-14">
                 </div>
@@ -26,7 +26,7 @@
                     </p>
                 </div>
             </a>
-            <div class="border border-black/30 rounded-md  flex items-center justify-center flex-col w-[23%]">
+            <div class="border border-black/30 rounded-md  flex items-center justify-center flex-col sm:w-[23%] w-[30%] mt-2">
                 <div class="p-2">
                     <img src="{{asset("capsule/images/list-factor.svg")}}" alt="" class="w-14 h-14">
                 </div>
@@ -38,7 +38,7 @@
                 </div>
             </div>
             <a href="{{route('admin.my-menu')}}"
-               class="border border-black/30 rounded-md  flex items-center justify-center flex-col w-[23%]">
+               class="border border-black/30 rounded-md  flex items-center justify-center flex-col sm:w-[23%] w-[30%] mt-2">
                 <div class="p-2">
                     <img src="{{asset("capsule/images/service.png")}}" alt="" class="w-14 h-14">
                 </div>
