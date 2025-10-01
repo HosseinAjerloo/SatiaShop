097efe99361aa2bc385169881ac799fc178bba56
HosseinAjerloo <ahosseinajerloo@gmail.com>
2025-10-01 11:30:36 +0330

    finishedBranch

--- app/Http/Controllers/Panel/PaymentController.php
+++ app/Http/Controllers/Panel/PaymentController.php
@@ -169,7 +169,6 @@ class PaymentController extends Controller
             }
 
             $back_price = $objBank->verify($payment->amount);
-            Log::emergency('bank price'.(string)$back_price);
             if ($back_price !== true or Payment::where("order_id", $payment->order_id)->count() > 1) {
 
                 $payment->update(
